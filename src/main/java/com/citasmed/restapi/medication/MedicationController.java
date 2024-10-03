95fdf5e51073a73b8d6bf69966e3f07f9bd620b6
Jozuan Lopez <jozuanlopez@gmail.com>
2024-10-02 23:33:13 -0600

    Implements the CRUD methods in MedicationController

--- src/main/java/com/citasmed/restapi/medication/MedicationController.java
+++ src/main/java/com/citasmed/restapi/medication/MedicationController.java
@@ -1,33 +1,51 @@
 package com.citasmed.restapi.medication;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/medications")
+@RequestMapping("/api/medications/")
 public class MedicationController {
+    @Autowired
+    private MedicationService medicationService;
 
     @GetMapping
-    public String getMedications() {
-        return "Get all medications";
+    public List<Medication> getMedications() {
+        return medicationService.findAll();
     }
 
     @GetMapping("/{id}")
-    public String getMedicationById(@PathVariable Integer id) {
-        return "Get medication with ID: " + id;
+    public ResponseEntity<?> getMedicationById(@PathVariable Integer id) {
+        Optional<Medication> medicationOpt = medicationService.findById(id);
+
+        if (medicationOpt.isEmpty()) {
+            Map<String, String> errorResponse = Map.of(
+                    "message", "Medication not found with ID: " + id);
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+
+        return ResponseEntity.ok(medicationOpt.get());
     }
 
     @PostMapping
-    public String createMedication() {
-        return "Create a new medication";
+    public Medication createMedication(@RequestBody Medication medication) {
+        return medicationService.save(medication);
     }
 
     @PutMapping("/{id}")
-    public String updateMedication(@PathVariable Integer id) {
-        return "Update medication with ID: " + id;
+    public Medication updateMedication(@PathVariable Integer id, @RequestBody Medication medication) {
+        return medicationService.updateMedication(id, medication);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteMedication(@PathVariable Integer id) {
-        return "Delete medication with ID: " + id;
+    public Medication deleteMedication(@PathVariable Integer id) {
+        return medicationService.deleteById(id);
     }
 }
